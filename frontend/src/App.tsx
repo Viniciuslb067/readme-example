26353f7c9135b94219247251384581cf8b86e2be
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2024-03-19 19:02:05 -0300

    chore: config my frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-[#E5E7EB]">
+      <h1>callface challenge</h1>
+    </div>
+  );
+}
+
+export default App;
