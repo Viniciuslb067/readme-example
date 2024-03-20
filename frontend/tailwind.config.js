04bca84445b7f20647977c0bfb836b8728c349a3
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2024-03-20 13:56:53 -0300

    feat: ensure support colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,10 +9,16 @@ export default {
           100: '#F4F4F4',
           200: '#E5E7EB',
           300: '#E4E4E4',
-          400: '#AFAFAF'
+          400: '#AFAFAF',
+          500: '#515151',
+          700: '#282828'
         },
         brand: {
           primary: '#E73C5B'
+        },
+        support: {
+          error: '#E73C3C',
+          success: '#30D158'
         }
       }
     }
