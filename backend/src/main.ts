adef1c0a3a9faa4d79b449a3fec1c836db1d78fe
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2024-03-20 19:08:33 -0300

    refactor: change backend boilerplate

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,43 +1,31 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-
-import { AppModule } from './modules/app/app.module';
-import { API_PREFIX } from './shared/constants/global.constants';
-import { SwaggerConfig } from './configs/config.interface';
-import { GLOBAL_CONFIG } from './configs/global.config';
-import { MyLogger } from './modules/logger/logger.service';
-import { InvalidFormExceptionFilter } from './filters/invalid.form.exception.filter';
-import { AllExceptionsFilter } from './filters/all.exceptions.filter';
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { AppModule } from './app.module';
+import type {
+  CorsConfig,
+  NestConfig,
+  SwaggerConfig,
+} from './common/configs/config.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'error', 'warn']
-  });
+  const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix(API_PREFIX);
-
-  app.useGlobalFilters(
-    new AllExceptionsFilter(app.get(HttpAdapterHost)),
-    new InvalidFormExceptionFilter()
-  );
-
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_URL,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      credentials: true
-    })
-  );
-
-  app.use(cookieParser());
-
+  // Validation
   app.useGlobalPipes(new ValidationPipe());
 
-  const configService = app.get<ConfigService>(ConfigService);
+  // enable shutdown hook
+  app.enableShutdownHooks();
+
+  // Prisma Client Exception Filter for unhandled exceptions
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  const configService = app.get(ConfigService);
+  const nestConfig = configService.get<NestConfig>('nest');
+  const corsConfig = configService.get<CorsConfig>('cors');
   const swaggerConfig = configService.get<SwaggerConfig>('swagger');
 
   // Swagger Api
@@ -46,17 +34,17 @@ async function bootstrap() {
       .setTitle(swaggerConfig.title || 'Nestjs')
       .setDescription(swaggerConfig.description || 'The nestjs API description')
       .setVersion(swaggerConfig.version || '1.0')
-      .addBearerAuth()
       .build();
     const document = SwaggerModule.createDocument(app, options);
 
     SwaggerModule.setup(swaggerConfig.path || 'api', app, document);
   }
 
-  const PORT = process.env.PORT || GLOBAL_CONFIG.nest.port;
-  await app.listen(PORT, async () => {
-    const myLogger = await app.resolve(MyLogger);
-    myLogger.log(`Server started listening: ${PORT}`);
-  });
+  // Cors
+  if (corsConfig.enabled) {
+    app.enableCors();
+  }
+
+  await app.listen(process.env.PORT || nestConfig.port || 3000);
 }
 bootstrap();
